a6114dbc2a212bfce62bff6d0bdcce3a0350d644
vrush1598 <vrushalimane9696@gmai.com>
2023-07-10 12:08:17 +0530

    THIS IS the branch mechanism

---
+++ list.py
@@ -0,0 +1,20 @@
+lis = [1, 23, 'python', 33, 'language']
+print(type(lis))
+
+print(len(lis))
+
+print(lis[2])
+
+print(lis[1:3])
+
+lis[1] = 'java'
+print(lis)
+
+lis.insert(2, 'aws10')
+print(lis)
+
+lis.append('html')
+print(lis)
+
+lis.reverse()
+print(lis)
