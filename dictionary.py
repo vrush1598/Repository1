1b33aefd16f224991dd1ecbbecc6c35cbdcef5d8
vrush1598 <vrushalimane9696@gmai.com>
2023-07-10 12:20:37 +0530

    THIS IS 2ND BRANCH MECHANISM

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dic = {"name": "Vrush", "age": 25, "address": 'Kolhapur'}
+print(type(dic))
+
+print(len(dic))
+
+print(dic.keys())
+
+print(dic.values())
+
+dic['name'] = 'go language'
+print(dic)
+
+dic.update({'popularity': 'very high'})
+print(dic)
