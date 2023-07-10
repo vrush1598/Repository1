07e1fa73d79fea937fd6b3ec0b1692c038e856cf
vrush1598 <vrushalimane9696@gmai.com>
2023-07-10 13:06:20 +0530

    THIS IS 4th BRANCH MECHANISM

---
+++ loop.py
@@ -0,0 +1,20 @@
+str = [10, 20, 30, 54.7, 'py']
+for i in str:
+    print(i)
+    if i == 30:
+        break
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+num = 0
+while num < 4:
+    num += 1
+    print('printing the statement')
+
+
+
+
+
+
